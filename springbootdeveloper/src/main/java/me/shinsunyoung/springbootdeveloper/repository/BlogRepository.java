21398b4d76b647fa0e87abe7de66bf7170973c22
tggKim <138564756+tggKim@users.noreply.github.com>
2024-01-15 19:28:36 +0900

    Delete springbootdeveloper directory

--- springbootdeveloper/src/main/java/me/shinsunyoung/springbootdeveloper/repository/BlogRepository.java
+++
@@ -1,9 +0,0 @@
-package me.shinsunyoung.springbootdeveloper.repository;
-
-import me.shinsunyoung.springbootdeveloper.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BlogRepository extends JpaRepository<Article,Long> {
-}
